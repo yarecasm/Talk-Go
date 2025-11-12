e27e51296f5aafe4e06252224ccf7b22f7b474e7
FerRodr <mariferrodriguez170604@gmail.com>
2025-11-11 20:18:09 -0600

    Pantalla de Categorias terminada

---
+++ Categorias/script.js
@@ -0,0 +1,16 @@
+function StartWindow() {
+    location.assign('../Inicio/index.html');
+}
+function BagelsWindow() {
+    location.assign('../Bagels/index.html');
+}
+function DrinksWindow() {
+    location.assign('../Drinks/index.html');
+}
+function SnacksWindow() {
+    location.assign('../Snacks/index.html');
+}
+function CombosWindow() {
+// location.assign('../Combos/index.html');
+}
+
